ab3e1bf085c2fb82597b9a89e4ac6b47f1ec0c25
drivera <DiegoEduardo.Rivera@cba.gov.ar>
2024-02-21 17:46:35 -0300

    Selenium testing clase 8

---
+++ src/test/java/com/drivera/clase8/pages/HomeTest.java
@@ -0,0 +1,24 @@
+package com.drivera.clase8.pages;
+
+import com.drivera.clase8.pages.HomePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeTest extends BaseTest {
+
+    @Test
+    public void validarHome() {
+        HomePage homePage = new HomePage(getDriver());
+
+        // Paso 1: levantar la pagina
+        getDriver().get("https://opencart.abstracta.us/");
+
+        // Paso 2: realizar las aserciones
+        Assert.assertTrue(homePage.inputIsDisplayed());
+        Assert.assertTrue(homePage.carrouselIsDisplayed());
+        Assert.assertTrue(homePage.menuIsDisplayed());
+        Assert.assertTrue(homePage.myAccountIsDisplayed());
+    }
+}
